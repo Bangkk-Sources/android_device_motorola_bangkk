249a94441bead9092ddcb8178482c6800fca16ed
ARYANGODxDOfficial <aryansanjay.080805@gmail.com>
2026-01-19 17:00:07 +0000

    bangkk: Bringup for LineageOs

--- lineage_bangkk.mk
+++ lineage_bangkk.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from bangkk device
 $(call inherit-product, device/motorola/bangkk/device.mk)
 
-# Inherit some common Euclid stuff.
-$(call inherit-product, vendor/euclid/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := euclid_bangkk
+PRODUCT_NAME := lineage_bangkk
 PRODUCT_DEVICE := bangkk
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
@@ -27,27 +27,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=motorola/bangkk_g/bangkk:15/V1TCS35H.88-16-1/d09472-674756:user/release-keys \
     DeviceProduct=bangkk_g
 
-
-EUCLID_MAINTAINER := ARYAN
-
-# Disable/enable blur support, false by default
-TARGET_ENABLE_BLUR := true
-
-
-#Gapps
-EUCLID_GAPPS := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true  
-TARGET_BUILD_BCR := true (For Basic Call Recorder)
-TARGET_BUILD_DOTGALLERY := true (For Including DotGallery)
-
-#UDFPS
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_HAS_UDFPS := true
-
-#Misc.
-TARGET_SUPPORTS_TOUCHGESTURES := true
-EUCLID_DEVICE := Moto_G84_5G
-EUCLID_PROCESSOR := Snapdragon_695
-EUCLID_BUILD_TYPE := OFFICIAL
